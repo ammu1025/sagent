66a8a1de2d396d1db611689f4fdf94f174ad2d3c
SWATHI A S <ammuswathi1025@gmail.com>
2026-03-24 11:03:24 +0530

    Delete habittracker directory

--- habittracker/habittracker/src/main/java/com/example/habittracker/repository/HabitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.habittracker.repository;
-
-import com.example.habittracker.model.Habit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface HabitRepository extends JpaRepository<Habit, Long> {
-    List<Habit> findByUserUserId(Long userId);
-}
--- habittracker/habittracker/src/main/java/com/example/habittracker/repository/HistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.habittracker.repository;
-
-import com.example.habittracker.model.History;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HistoryRepository extends JpaRepository<History, Long> {
-}
